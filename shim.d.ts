d16167a550c881a9c10af916915deb53e6379018
Pig Fang <g-plane@hotmail.com>
2021-01-01 16:48:00 +0800

    tweak Prettier config

--- shim.d.ts
+++ shim.d.ts
@@ -3,11 +3,11 @@ import type { ParseSelector } from './parser'
 declare global {
   interface ParentNode {
     querySelector<S extends string, E extends Element = ParseSelector<S>>(
-      selector: S
+      selector: S,
     ): E | null
 
     querySelectorAll<S extends string, E extends Element = ParseSelector<S>>(
-      selector: S
+      selector: S,
     ): NodeListOf<E>
   }
 }
