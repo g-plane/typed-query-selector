503e2ba58c304ef755e74964ac687878707d18f6
Federico Brigante <me@fregante.com>
2022-12-03 15:55:08 +0800

    Add Fallback support to `StrictlyParseSelector`

--- parser.d.ts
+++ parser.d.ts
@@ -221,13 +221,16 @@ type IsValidRestChars<S extends string> = S extends `${infer H}${infer Rest}`
     : false
   : true // no characters left, so it's OK
 
-export type StrictlyParseSelector<S extends string> =
+export type StrictlyParseSelector<
+  S extends string,
+  Fallback extends Element = Element,
+> =
   ParseSelectorToTagNames<S> extends infer Tags
     ? Tags extends []
-      ? TagNameToElement<''>
+      ? TagNameToElement<'', Fallback>
       : Tags extends string[]
       ? IsValidTags<Tags> extends true
-        ? TagNameToElement<Tags[number]>
+        ? TagNameToElement<Tags[number], Fallback>
         : never
       : never
     : never
